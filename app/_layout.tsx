2d0d0272cd9168ea1419d6dd2633bd7b8a0fdb9e
Sushabhan <sushabhanmajumder878@gmail.com>
2025-07-22 09:50:05 +0530

    Implimented the custom font family

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,22 @@
-import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import { SplashScreen, Stack } from "expo-router";
+import { useEffect } from "react";
 import "./global.css";
 
 export default function RootLayout() {
-  return <Stack />;
+  const [fontsLoaded, error] = useFonts({
+    "Quicksand-Bold": require("../assets/fonts/Quicksand-Bold.ttf"),
+    "Quicksand-Medium": require("../assets/fonts/Quicksand-Medium.ttf"),
+    "Quicksand-SemiBold": require("../assets/fonts/Quicksand-SemiBold.ttf"),
+    "Quicksand-Regular": require("../assets/fonts/Quicksand-Regular.ttf"),
+    "Quicksand-Light": require("../assets/fonts/Quicksand-Light.ttf"),
+    // Add other font variants if needed
+  });
+  useEffect(() => {
+    if (error) throw error
+    if (fontsLoaded) SplashScreen.hideAsync()
+  }, [fontsLoaded, error]);
+  return <Stack screenOptions={{
+    headerShown: false,
+  }}/>;
 }
--- app/index.tsx
+++ app/index.tsx
@@ -4,7 +4,7 @@ import "./global.css";
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-red-100">
+      <Text className="text-5xl text-center text-primary font-quicksand-bold">
         Welcome to Nativewind!
       </Text>
     </View>
