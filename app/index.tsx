9bbcb723a6cd70d829684883c3b5127f7fb89d60
Sushabhan <sushabhanmajumder878@gmail.com>
2025-07-22 09:29:35 +0530

    Added constants images.d.ts and updated the tailwind.confic.ts

--- app/index.tsx
+++ app/index.tsx
@@ -4,7 +4,7 @@ import "./global.css";
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">
+      <Text className="text-xl font-bold text-red-100">
         Welcome to Nativewind!
       </Text>
     </View>
